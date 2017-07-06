9d32249deac11927ed308e1603fc5e2fcbf43041
MAOKG <maosen1993@gmail.com>
2017-07-06 21:37:07 +0000

    Add landing page

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 var express = require('express')
 var app = express()
 
-app.listen(3000)
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  res.render('landing')
+})
+
+app.get('/:time', function(req, res) {
+  var time = req.params.time
+  var obj = {'unix': null, 'natural': null}
+  res.send(obj)
+})
+
+app.get('*', function(req, res) {
+  res.send('You are in the wrong path!')
+})
+
+
+app.listen(3000, function() {
+  console.log('The server has started!')
+})
